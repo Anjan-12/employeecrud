c7ff1a6b52caa9f22bab422ad30c2a2ef8782a27
anjanpaudel <anjan.paudel96@gmail.com>
2021-04-12 18:31:05 +0545

    mernstack created

---
+++ Routes/Employee.route.js
@@ -0,0 +1,69 @@
+const express = require('express');
+
+ const app = express();
+ const employeeRoute = express.Router();
+ 
+
+ let employeeModel = require('../Model/Employee');
+ 
+
+ employeeRoute.route('/').get(function (req, res) {
+ employeeModel.find(function (err, employee) {
+ if (err) {
+ console.log(err);
+ }
+ else {
+ res.json(employee);
+ }
+ });
+ });
+ 
+ 
+ employeeRoute.route('/addEmployee').post(function (req, res) {
+ let employee = new employeeModel(req.body);
+ employee.save()
+ .then(game => {
+ res.status(200).json({ 'employee': 'Employee Added Successfully' });
+ })
+ .catch(err => {
+ res.status(400).send("Something Went Wrong");
+ });
+ });
+ 
+ 
+ employeeRoute.route('/editEmployee/:id').get(function (req, res) {
+ let id = req.params.id;
+ employeeModel.findById(id, function (err, employee) {
+ res.json(employee);
+ });
+ });
+ 
+ 
+ employeeRoute.route('/updateEmployee/:id').post(function (req, res) {
+ employeeModel.findById(req.params.id, function (err, employee) {
+ if (!employee)
+ return next(new Error('Unable To Find Employee With This Id'));
+ else {
+ employee.firstName = req.body.firstName;
+ employee.lastName = req.body.lastName;
+ employee.email = req.body.email;
+ employee.phone = req.body.phone;
+ 
+ employee.save().then(emp => {
+ res.json('Employee Updated Successfully');
+ })
+ .catch(err => {
+ res.status(400).send("Unable To Update Employee");
+ });
+ }
+ });
+ });
+
+ employeeRoute.route('/deleteEmployee/:id').get(function (req, res) {
+ employeeModel.findByIdAndRemove({ _id: req.params.id }, function (err, employee) {
+ if (err) res.json(err);
+ else res.json('Employee Deleted Successfully');
+ });
+ });
+ 
+ module.exports = employeeRoute;
